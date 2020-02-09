c69fb231fe309412676d793c3c8afd6e38a42a89
sanya-shch <shchipkov.sanya@ukr.net>
2020-02-09 18:57:17 +0200

    added the ability to add lists to the state.

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import classNames from 'classnames';
 import Badge from '../Badge';
+import removeSvg from '../../assets/img/remove.svg';
 import './List.scss';
 
-const List = ({ items, isRemovable, onClick }) => {
+const List = ({ items, isRemovable, onClick, onRemove }) => {
+  const removeList = item => {
+    if (window.confirm('Вы действительно хотите удалить список?')) {
+      onRemove(item);
+    }
+  };
+
   return (
     <ul className="list" onClick={onClick}>
       {
@@ -21,6 +28,16 @@ const List = ({ items, isRemovable, onClick }) => {
               }
             </i>
             <span>{item.name}</span>
+            {
+              isRemovable && (
+                <img
+                  className="list__remove-icon"
+                  src={removeSvg}
+                  alt="Remove icon"
+                  onClick={() => removeList(item)}
+                />
+              )
+            }
           </li>
         ))
       }
